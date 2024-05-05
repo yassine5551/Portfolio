9511d57559c28d4c747562362eb48d080f0e6352
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-05-05 23:40:02 +0000

    update intro section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Experience from "./Components/Experience/Experience";
 import Navbar from "./Components/Navbar/Navbar";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Works from "./Components/Work/Works";
-import Intro from "./Components/intro/Intro";
+import Intro from "./Components/Intro/Intro";
 import Services from "./Components/services/Services";
 import './app.css'
 function App() {
