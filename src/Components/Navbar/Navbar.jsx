1db2368ba73150502f16b206116b8825ca899755
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-05-05 15:27:01 +0000

    NavBar section done

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+
+import './Navbar.css'
+function Navbar() {
+  return (
+      <div className="n-wrapper">
+          <div className="n-left">
+              <div className='n-name'>Yassine</div>
+              <span>toggle</span>
+          </div>
+          <div className="n-right">
+              <div className='n-list'>
+                  <ul style={{listStyleType:'none'}}>
+                      <li>Home</li>
+                      <li>Services</li>
+                      <li>Experience</li>
+                      <li>Portfolio</li>
+                      <li>Testimonials</li>
+                      
+                  </ul>
+              </div>
+              <button className="button n-button">
+                  contact us
+              </button>
+          </div>
+    </div>
+  )
+}
+
+export default Navbar
