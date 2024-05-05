5c3359aff958a063dea57d73d3706729421df9c2
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-05-05 17:38:07 +0000

    set up in services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Intro from "./Components/intro/Intro";
+import Services from "./Components/services/Services";
 import './app.css'
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Intro/>
+      <Intro />
+      <Services/>
     </div>
   );
 }
---
+++ src/Components/services/Services.jsx
@@ -0,0 +1,9 @@
+
+import './services.css'
+function Services() {
+  return (
+    <div>Services</div>
+  )
+}
+
+export default Services
