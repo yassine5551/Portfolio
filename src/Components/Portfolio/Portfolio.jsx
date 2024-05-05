ff8ffdab62bac7fa381f08a0bbed481b19ca5bc6
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-05-05 19:21:24 +0000

    Work section done

---
+++ src/Components/Portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import './Portfolio.css'
+const Portfolio = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Portfolio
--- src/Components/Work/Works.jsx
+++ src/Components/Work/Works.jsx
@@ -1,4 +1,9 @@
-import './Works.css'
+import "./Works.css";
+import Upwork from "../../img/Upwork.png";
+import Fiverr from "../../img/fiverr.png";
+import Amazon from "../../img/amazon.png";
+import Shopify from "../../img/Shopify.png";
+import Facebook from "../../img/Facebook.png";
 const Works = () => {
   return (
     <div className="works">
@@ -9,7 +14,10 @@ const Works = () => {
         <span>
           I worked for Vala blue Company .
           <br />
-          And As A Freelancer I have been developing websites and apps for Restaurants
+          And As A Freelancer I have been developing
+          <br />
+          websites and apps
+          for Restaurants
           <br />
           and for Small Businesses for the past 2 years .
         </span>
@@ -17,6 +25,29 @@ const Works = () => {
         <button className="button s-button">Hire me </button>
 
         <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* RightSide */}
+      <div className="w-right">
+        <div className="w-mainCircle">
+          <div className="w-secCircle">
+            <img src={Upwork} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Fiverr} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Amazon} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Shopify} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Facebook} alt="" />
+          </div>
+          {/* backGroundCircles */}
+          <div className="w-backCircle blueCircle"></div>
+          <div className="w-backCircle yellowCircle"></div>
+        </div>
       </div>
     </div>
   );
