fd62d2535acac41180990ade2270797de4d68dba
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-05-05 16:18:25 +0000

    right side intro section done

--- src/Components/intro/intro.jsx
+++ src/Components/intro/intro.jsx
@@ -1,7 +1,22 @@
 import './intro.css'
+
+//images for left side
 import Github from '../../img/github.png'
 import Linkedin from '../../img/linkedin.png'
 import Instagram from '../../img/instagram.png'
+//end ofimages for left side
+
+//images for right side
+import Vector1 from '../../img/Vector1.png'
+import Vector2 from '../../img/Vector2.png'
+import boy from '../../img/boy.png'
+import thumbup from '../../img/thumbup.png'
+import Crown from '../../img/crown.png'
+import glassesemoji from '../../img/glassesimoji.png'
+import FloatingDiv from '../floatingDiv/FloatingDiv'
+
+
+
 function Intro() {
   return (
       <div className="intro">
@@ -32,7 +47,15 @@ function Intro() {
               
           </div>
           <div className="i-right">
-              im right side
+              <img src={Vector1} alt="" />
+              <img src={Vector2} alt="" />
+              <img src={boy} alt="" />
+              <div style={{top:'-4%', left:'68%'}}>
+                  <FloatingDiv image={Crown} txt1='Web' txt2='Developper' />
+              </div>
+              <div style={{top:'18rem', left:'0rem'}}>
+                  <FloatingDiv image={thumbup} txt1='Best Design' txt2='Awards' />
+              </div>
           </div>
     </div>
   )
