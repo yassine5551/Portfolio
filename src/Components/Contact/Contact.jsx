9d4121e67a80a7c9d1959c077d38d2af2646806a
Yassine_Elkoujjer <yassin123beta@gmail.com>
2024-05-07 23:13:29 +0000

    Contact section done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,29 @@
-
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 import "./Contact.css";
 
 const Contact = () => {
+    const form = useRef();
+    const [done, setDone] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+  
+        emailjs
+            .sendForm('service_4iapan4', 'template_r1v9xsh', form.current, {
+                publicKey: 'Qeg9LwtRSo-r5xkyG',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    setDone(true);
+                    form.current.reset();
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    }
   return (
     <div className="contact-form">
       <div className="w-left">
@@ -19,11 +41,12 @@ const Contact = () => {
           
 
           <div className="c-right">
-              <form >
+              <form ref={form} onSubmit={sendEmail}>
                   <input type="text" name="user_name" className="user" placeholder="Name" />
                   <input type="email" name="user_email" className="user" placeholder="Email" />
                   <textarea name="message" className="user" placeholder="Message"></textarea>
                   <input type="submit" value="send" className="button" />
+                  <span>{done && "Thanks for Contacting me"}</span>
                   <div className="blur c-blur1" style={{background: "var(--purple)"}}></div>
               </form>
           </div>
