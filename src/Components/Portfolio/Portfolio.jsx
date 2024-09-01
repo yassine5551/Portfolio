c38dc8bca500e0e9197ce67587984552f326e20b
elkoujjer yassine <yassin123beta@gmail.com>
2024-09-02 00:54:11 +0100

    adding project to portfolio section

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,10 +1,9 @@
 import "./Portfolio.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import masterChef from "../../img/p1.png";
 import typing from "../../img/typing.png";
 import WebAgency from "../../img/web.png";
-import Hoc from "../../img/hoc.png";
-import restaurantImg from "../../img/restaurant_v1.png"
+import restaurantImg from "../../img/restaurant_v1.png";
 import "swiper/css";
 import { themeContext } from "../../../context";
 import { useContext } from "react";
@@ -20,26 +19,33 @@ const Portfolio = () => {
 
       {/* slider */}
       <Swiper
-       
         spaceBetween={30}
         slidesPerView={3}
         grabCursor={true}
         className="portfolio-slider"
       >
         <SwiperSlide>
-          
-          <a href="https://restaurant-v1-five.vercel.app/" target="_blank"><img src={restaurantImg} alt="" /></a>
+          <a
+            href="https://restaurant-landing-page-961q5q529-el-koujjer-yassines-projects.vercel.app/"
+            target="_blank"
+          >
+            <img src={masterChef} alt="masterchef" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-          <a href="https://typing-game-tawny.vercel.app/" target="_blank"><img src={typing} alt="" /></a>
-          
+          <a href="https://restaurant-v1-five.vercel.app/" target="_blank">
+            <img src={restaurantImg} alt="" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-        <a href="https://web-agency-delta.vercel.app/" target="_blank" ><img src={WebAgency} alt=""  /></a>
-          
+          <a href="https://web-agency-delta.vercel.app/" target="_blank">
+            <img src={WebAgency} alt="" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Hoc} alt="" />
+          <a href="https://typing-game-tawny.vercel.app/" target="_blank">
+            <img src={typing} alt="" />
+          </a>
         </SwiperSlide>
       </Swiper>
     </div>
